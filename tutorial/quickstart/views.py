e09b4bbb286b4974c549ce8ba7a9967071ed3310
Jonatas E S Silva <jonatassilvaemanuel@gmail.com>
2023-03-17 23:53:04 -0300

    quickstart documentation tutorial

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.model import Group, User
+from rest_framework import permissions, viewsets
+
+from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
+
+
+class UserViewSet(viewsets.ModekViewSet):
+    """
+    API endpoint that allows users to be viewd or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permissions_class = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows group to be viewd or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,4 +1,4 @@
-"""backend URL Configuration
+"""tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+
+# from django.contrib import admin
+from tutorial.quickstart import views
+
+routers = routers.DefaultRouter()
+routers.register(r'users', views.UserViewSet)
+routers.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [
-    path('', include('todo.urls')),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('', include('router.urls')),
+    # path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
